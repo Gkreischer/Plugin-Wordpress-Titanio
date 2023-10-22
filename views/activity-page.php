1fbb8daf56e53f699c2a9253f754f1b8b459e0b8
Gustavo Kreischer de Almeida <gustavokreischer@gmail.com>
2023-10-22 19:49:32 -0300

    add: post activities almost finished

--- views/activity-page.php
+++ views/activity-page.php
@@ -61,26 +61,27 @@
         </div>
     </section>
     <!-- Modal -->
-    <div class="modal fade bg-dark" id="modal-adicionar-atividade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Nova Atividade</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form id="form-activity">
+    <form id="form-activity">
+
+        <div class="modal fade bg-dark" id="modal-adicionar-atividade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Nova Atividade</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
                         <div class="mb-3">
-                            <label for="name" class="form-label fw-bold">Nome</label>
-                            <input type="text" class="form-control" id="name" name="name">
+                            <label for="name" class="form-label fw-bold">Autor</label>
+                            <input type="text" class="form-control" id="name" name="author" required minlength="3" maxlength="100">
                         </div>
                         <div class="mb-3">
                             <label for="date" class="form-label fw-bold">Data</label>
-                            <input type="date" class="form-control" id="date" name="date">
+                            <input type="date" class="form-control" id="date" name="date" required>
                         </div>
                         <div class="mb-3">
                             <label for="description" class="form-label fw-bold" for="description">Descrição</label>
-                            <textarea class="form-control" id="description" rows="5" name="description"></textarea>
+                            <textarea class="form-control" id="description" rows="5" name="description" required></textarea>
                         </div>
                         <div class="mb-3 form-check">
                             <div class="row ">
@@ -102,15 +103,31 @@
                             <label for="obs" class="form-label fw-bold" for="obs">OBS</label>
                             <textarea class="form-control" id="obs" rows="5" name="obs"></textarea>
                         </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                    <button type="button" class="btn btn-primary" id="save-activity">Salvar</button>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        <button type="submit" class="btn btn-primary" id="save-activity">Salvar</button>
+                    </div>
                 </div>
             </div>
         </div>
+    </form>
+
+    <!-- Fim Modal -->
+    <!-- Toast -->
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <strong class="me-auto">Bootstrap</strong>
+                <small>11 mins ago</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                Hello, world! This is a toast message.
+            </div>
+        </div>
     </div>
+    <!-- Fim Toast -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -119,17 +136,39 @@
     <script>
         jQuery(document).ready(function($) {
 
+            function showToast(toastElement) {
+                var toastElement = $('#liveToast');
+                const toastBootstrap = new bootstrap.Toast(toastElement[0]);
+                toastBootstrap.show();
+            }
 
-            $('#save-activity').on('click', function() {
-                var form = $('#form-activity').serialize();
+            $('#form-activity').submit(function() {
+                event.preventDefault();
+                var form = $(this).serialize();
                 console.log(form);
 
                 $.ajax({
-                    method: 'POST',
-                    url: '<?php echo get_rest_url(null, 'central-agencia-titanio/v1/activities') ?>',
-                    data: form
+                    method: 'post',
+                    url: '<?php echo get_rest_url(null, '/central-agencia-titanio/v1/activities/create') ?>',
+                    headers: {
+                        'X-Wp-Nonce': '<?php echo wp_create_nonce('wp_rest') ?>'
+                    },
+                    data: form,
+                    success: function(response) {
+                        console.log(response);
+                        location.reload();
+                    },
+                    error: function(error) {
+                        console.error(error);
+                    },
+                    complete: function() {
+                        showToast();
+
+                    }
                 });
             });
+
+
 
         });
     </script>
